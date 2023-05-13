ca37ee6f823f74f489f4f64386700f1cb3d82e47
saityalcn <stsaityalcin@gmail.com>
2023-05-13 16:34:29 +0300

    fix minor bugs

--- src/main/java/com/humanresources/webservice/auth/AuthController.java
+++ src/main/java/com/humanresources/webservice/auth/AuthController.java
@@ -7,9 +7,11 @@ import com.humanresources.webservice.user.UserService;
 import com.humanresources.webservice.user.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class AuthController {
 
--- src/main/java/com/humanresources/webservice/workers/WorkersRepository.java
+++ src/main/java/com/humanresources/webservice/workers/WorkersRepository.java
@@ -1,8 +1,12 @@
 package com.humanresources.webservice.workers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WorkersRepository extends JpaRepository<Workers, Long> {
 
-
+    @Query(nativeQuery = true, value = "select * from workers where project_id=0")
+    List<Workers> findFreeWorkers();
 }
